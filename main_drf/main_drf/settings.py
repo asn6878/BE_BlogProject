050b4c65445d966ecc9e38da210c0920b0e943ca
Dinosy <asn6878@gmail.com>
2023-06-29 20:52:42 +0900

    fix : settings.py 변수 문제 해결

--- main_drf/main_drf/settings.py
+++ main_drf/main_drf/settings.py
@@ -8,9 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_FILE = mysettings.SECRET_KEY
-
-SECRET_KEY = mysettings.
+SECRET_KEY = mysettings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
